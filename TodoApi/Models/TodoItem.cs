4fee139dce851e3365b07570cd4394cf76bbcdd0
Antonio <antoniotmll@gmail.com>
2021-07-30 13:23:14 +0200

    ✨updated PostTodoItem

--- TodoApi/Models/TodoItem.cs
+++ TodoApi/Models/TodoItem.cs
@@ -1,9 +1,10 @@
 namespace TodoApi.Models
 {
-    public class TodoItems
-    {
+    public class TodoItem
+        {
         public long Id{get; set;}
         public string Name {get; set;}
         public bool IsComplete{get; set;}
+        
     }
 }
